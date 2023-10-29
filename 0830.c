0b3529dd6f9135cf7e32fa81ccd99a0a3e10607d
BIT230192@st.cmc-u.edu.vn <BIT230192@st.cmc-u.edu.vn>
2023-10-29 20:35:17 +0700

    ghi chu vao day

--- 0830.c
+++ 0830.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(){
-//	int n;
-//	scanf("%d", &n);
-//	 
-//	if((n%3==0)&&(n%5==0)){
-//		printf("1"); 
-//	} 
+	int k;
+	scanf("%d", &k);
+	 
+	if((k%3==0)&&(k%5==0)){
+		printf("1"); 
+	} 
 	printf("hahaha"); 
 	return 0; 
 } 
