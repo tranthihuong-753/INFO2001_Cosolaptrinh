b7f7613452cf167b4045fb7010acc81fe74bca28
BIT230192@st.cmc-u.edu.vn <BIT230192@st.cmc-u.edu.vn>
2023-10-30 12:30:32 +0700

    ghi chu vao day

--- BT.c
+++ BT.c
@@ -1,21 +1,8 @@
-/*
-viet chuong trinh duyet mat khau 
-neu nho hon 15 ky tu thi phai tm : tren 8 ky tu, co 1 chu in hoa, 1 chu in thuong, 1 so , q ki tu  
-*/
-
-
 #include <stdio.h>
-#include <ctype.h>
 
 int main() {
-    char str[] = "Hello!@#123";
-    int i;
-
-    for (i = 0; str[i] != '\0'; i++) {
-        if (!isalnum(str[i])) {
-            printf("Chu?i k? t? ch?a k? t? ð?c bi?t: %c\n", str[i]);
-        }
-    }
+	printf("hahiha"); 
+	return 0; 
 }
 
  
