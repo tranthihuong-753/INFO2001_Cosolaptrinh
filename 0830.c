5aa0a41510f4019eef7527661ea82e5501c322f6
BIT230192@st.cmc-u.edu.vn <BIT230192@st.cmc-u.edu.vn>
2023-10-30 09:01:05 +0700

    ghi chu vao day

--- 0830.c
+++ 0830.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main(){
-	int k;
-	scanf("%d", &k);
-	printf("%d",k); 
 
-    if(k<0){
+	printf("k"); 
+	printf("hiahh"); 
+	    if(1<0){
      	printf("k am"); 
 	 }else{
 	 	printf("k khong am"); 
