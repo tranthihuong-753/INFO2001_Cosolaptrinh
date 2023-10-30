8f617063c2b29aec2921ba82e1625791786d09c3
BIT230192@st.cmc-u.edu.vn <BIT230192@st.cmc-u.edu.vn>
2023-10-30 10:29:46 +0700

    ghi chu vao day

--- 0830.c
+++ 0830.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 
 int main(){
-	int n;
-	scanf("%d", &n);
-	double s;
-	int k;	
-	 
-	for(k=0;k<=n;k=k+1){
-		s=k*((-1)^(k+1));
-		s+=s; 
-	} 
-	printf("%.2lf", s);
+	printf("hah");
 	return 0; 
 }
