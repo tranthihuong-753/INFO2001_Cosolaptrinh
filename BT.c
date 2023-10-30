6c760ed2c9abeb3cc01a6511dfb37d60607e5225
Trần Thị Hường <BIT230192@st.cmc-u.edu.vn>
2023-10-30 12:35:52 +0700

    ghi chu vao day

--- BT.c
+++ BT.c
@@ -1,11 +1,8 @@
-/*
-viet chuong trinh duyet mat khau 
-neu nho hon 15 ky tu thi phai tm : tren 8 ky tu, co 1 chu in hoa, 1 chu in thuong, 1 so , q ki tu  
-*/
-
 #include <stdio.h>
-int main(){
-	
-} 
+
+int main() {
+	printf("hahiha"); 
+	return 0; 
+}
 
  
