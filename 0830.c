2df64a696bc21a697e2479d3d8109eaf9b66f3e8
Trần Thị Hường <BIT230192@st.cmc-u.edu.vn>
2023-10-30 08:45:50 +0700

    ghi chu vao day

---
+++ 0830.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int k;
+	scanf("%d", &k);
+	printf("%d",k); 
+//	printf("hahaha"); 
+    printf("chothatchu"); 
+//    printf("ha 1");
+     if(k<0){
+     	printf("k am"); 
+	 } 
+	 
+	return 0; 
+} 
